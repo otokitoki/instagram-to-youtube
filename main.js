e50cd8796f5ed0dcb7b9c92fb9a31d5b5a941061
otokitoki <53075562+otokitoki@users.noreply.github.com>
2022-03-15 00:22:04 +0900

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ window.onload = function() {
   const afterParam = beforeParam.replace("q=https://youtu.be/", "");
   
   // カスタムURLスキーム
-  let url = 'youtube://youtube.com/' + param; 
+  let url = 'javascript:location.protocol="youtube:' + param; 
   location.href = url;
   
   document.write(url)
