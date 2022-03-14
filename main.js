bc816624e686b9b1f2a87e757866ccadd8c7fa96
otokitoki <53075562+otokitoki@users.noreply.github.com>
2022-03-15 00:12:01 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 window.onload = function() {
   // URLパラメータ文字列を取得する
-  const param = location.search;
+  const beforeParam = location.search;
+  const afterParam = beforeParam.replace("https://youtu.be/", "");
+  
   // カスタムURLスキーム
-  let url = 'youtube://' + param; 
+  let url = 'youtube://youtube.com/' + param; 
   location.href = url;
+  
+  document.write(url)
 }
